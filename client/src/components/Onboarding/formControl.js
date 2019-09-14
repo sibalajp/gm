9984ece2f241f14db2d44eff66c29921ef671d00
sibalajp <sibalajp@gmail.com>
2019-09-13 19:56:58 -0700

    refactored registration form

---
+++ client/src/components/Onboarding/formControl.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import Form from "react-bootstrap/Form";
+
+class FormControl extends Component {
+  render() {
+    console.log(this.props);
+    return <Form.Control></Form.Control>;
+  }
+}
+
+export default FormControl;
--- client/src/components/Onboarding/registration-form.js
+++ client/src/components/Onboarding/registration-form.js
@@ -1,105 +1,89 @@
-import React, { Component } from 'react';
-import {connect} from 'react-redux';
-import {Field, reduxForm} from 'redux-form';
-import './registration-form.css';
-import {registerUser} from '../../actions/user-crud';
-import {login} from '../../actions/auth';
-import Input from './input';
-
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Field, reduxForm } from "redux-form";
+import "./registration-form.css";
+import { registerUser } from "../../actions/user-crud";
+import { login } from "../../actions/auth";
+import FormControl from "./formControl";
+import Form from "react-bootstrap/Form";
 
 export class RegistrationForm extends Component {
+  componentDidMount() {
+    document.title = "Register";
+  }
 
-    componentDidMount() {
-        document.title = "Register"
+  onSubmit = values => {
+    const { email, password } = values;
+    const user = { email, password };
+
+    return this.props
+      .dispatch(registerUser(user))
+      .then(() => this.props.dispatch(login(email, password)));
+  };
+
+  render() {
+    let error;
+    const email = value =>
+      value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
+        ? "Invalid email address"
+        : undefined;
+    // const required = value => (value ? undefined : "Required");
+    // const minValue = min => value =>
+    //   value && value < min ? `Must be at least ${min}` : undefined;
+    // const minValue8 = minValue(8);
+
+    if (this.props.error) {
+      error = (
+        <div className="form-error" aria-live="polite">
+          {this.props.error}
+        </div>
+      );
     }
 
-    onSubmit(values) {
-        const {email, password} = values;
-        const user = {email, password};
-
-        return this.props
-            .dispatch(registerUser(user))
-            .then(() => this.props.dispatch(login(email, password)));
-    }
-
-
-    render() {
-        let error;
-        if (this.props.error) {
-            error = (
-                <div className="form-error" aria-live="polite">
-                    {this.props.error}
-                </div>
-            );
-        }
-
-        return (
-            <form
-                className=""
-                id="register"
-                onSubmit={this.props.handleSubmit(values =>
-                    this.onSubmit(values)
-                )}>
-                <h2>Register</h2>
-
-                {error}
-                
-                <Field
-                    component={Input}
-                    type="text"
-                    name="username"
-                    label = "Username:"
-                    className="required"
-                />
-                <Field 
-                    component={Input} 
-                    type="text" 
-                    name="firstName" 
-                    label="First Name:"
-                    className="required"
-                    maxLength="12"
-                  
-                />
-                <Field 
-                    component={Input} 
-                    type="text" 
-                    name="lastName" 
-                    label="Last Name:"
-                    className="required"
-                   
-                />
-                <Field
-                    component={Input}
-                    type="password"
-                    name="password"
-                    label="Password:"
-                    className="required"
-                   
-                />
-                <Field
-                    component={Input}
-                    type="password"
-                    label="Confirm Password:"
-                    name="passwordConfirm"
-                    className="required"
-              
-                />
-                <button
-                    type="submit"
-                >
-                    Register
-                </button>
-            </form>
-        );
-    }
-
-
-
-
+    return (
+      <Form id="register" onSubmit={this.props.handleSubmit(this.onSubmit)}>
+        <h2>Register</h2>
+        {error}
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Field
+            component={FormControl}
+            type="text"
+            name="email"
+            validate={email}
+          />
+        </Form.Group>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Password</Form.Label>
+          <Field
+            component={FormControl}
+            type="password"
+            name="password"
+            // validate={[required]}
+          />
+        </Form.Group>
+      </Form>
+    );
+  }
 }
 
+const validate = formValues => {
+  const errors = {};
 
-export default connect()(reduxForm({
-    form: 'registration',
-    
-})(RegistrationForm));
+  if (!formValues.title) {
+    errors.title = "You must enter a title";
+  }
+
+  if (!formValues.description) {
+    errors.description = "You must enter a description";
+  }
+
+  return errors;
+};
+
+export default connect()(
+  reduxForm({
+    form: "registration",
+    validate
+  })(RegistrationForm)
+);
